cc8f0061c2449b6161fb619adbb5fab8ec2efe2e
DevCarlos2641 <dev.carlos2641@outlook.com>
2025-06-10 12:45:42 -0600

    change test

--- src/main/java/com/carlos2641/system/domain/port/in/ArticleUseCase.java
+++ src/main/java/com/carlos2641/system/domain/port/in/ArticleUseCase.java
@@ -1,6 +1,5 @@
 package com.carlos2641.system.domain.port.in;
 
-import com.carlos2641.system.domain.model.Article;
 import com.carlos2641.system.infrastructure.in.dto.ArticlePreviewResponse;
 
 import java.util.List;
